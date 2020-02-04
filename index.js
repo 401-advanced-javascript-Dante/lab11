10195856980ce74166bea6e0e1fc0003cc80f5c7
dantemessy <thepunishar1@gmail.com>
2020-02-04 11:03:36 +0200

    adding to database fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 const server = require('./lib/server.js');
 const mongoose = require('mongoose');
-const MONGODB_URI = 'mongodb://localhost:27017/lab11';
+const MONGODB_URI = 'mongodb://localhost:27017/lab1111';
 
 const mongooseOptions = {
   useNewUrlParser: true ,
